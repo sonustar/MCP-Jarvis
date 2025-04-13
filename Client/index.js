c1cfa8b2e6f995cb384fc737d943682cee970ab4
Supratick Mondal <sonumondal5006@gmail.com>
2025-04-14 00:21:07 +0530

    Created a client that interacts with AI from the CLI .

--- Client/index.js
+++ Client/index.js
@@ -1,15 +1,78 @@
 require('dotenv').config();
+const readline = require('readline').promises; // this to take input from the CLI .
+
 const { GoogleGenAI } = require("@google/genai");
+const { text } = require('stream/consumers');
+const { type } = require('os');
+const { exit } = require('process');
 
 const ai = new GoogleGenAI({ apiKey: process.env.YOUR_API_KEY });
 
-async function main() {
-  // console.log(ab.parsed. YOUR_API_KEY)
+// Basically will hold all the past interactions with the user . 
+/* It is a empty array of objects of the past chat !!  */
+let chatHistory = []
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+async function chat() {
+
+  const question = await rl.question('You: ')
+
+  if(question.toLowerCase() == 'exit'){
+    console.log("🤖 AI:👋 Bye !! Have a nice day !!");
+      rl.close();
+      return
+      
+  }
+
+  chatHistory.push({
+    role : "user",
+    parts : [
+      {
+        text : question,
+        type:"text"
+      }
+    ]
+  })
+
+
+/* 
+response type object 
+
+keys :
+model : model-name ,
+content : which is from the user end 
+
+
+*/
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Explain how AI works",
+    contents: chatHistory,
   });
-  console.log(response.text);
+
+  // response.candidates[0].content.parts[0].text) -- to access the data given by the AI !! 
+  const aiText = response.candidates[0].content.parts[0].text
+
+
+  console.log("🤖 AI:",aiText);
+
+  chatHistory = [...chatHistory, {
+      role : "Model",
+      parts : [
+        {
+          text : aiText,
+          type : "text"
+        }
+      ]
+  }]
+
+
+  chat()
+  
 }
 
- main();
+chat()
